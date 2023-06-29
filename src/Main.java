b5545c66af4171c0c90e07abf5d2b6268fb2b998
Denis Skidan <skidan147@gmail.com>
2023-06-29 18:10:49 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 5000;
+        int mile = 20;
+        int bonus = price / mile;
+        System.out.println("Bonus miles charged:" + bonus);
+    }
+}
